aed45c5821e0b454081d79ae873a347d6135e45d
Sabari Doss R <sabursd18@gmail.com>
2025-05-13 23:14:59 +0530

    polymorphism : operator loading

---
+++ polymorphism/operatorOverloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+    int real, imag;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    
+    Complex operator+(const Complex& other) {
+        Complex result;
+        result.real = real + other.real;
+        result.imag = imag + other.imag;
+        return result;
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex a(2, 3);
+    Complex b(4, 5);
+
+    Complex c = a + b;  
+    c.display();        
+}
