ca33ef4c541dad6188448254dca19c2c0fb798d6
Sabari Doss R <sabursd18@gmail.com>
2025-05-13 21:52:08 +0530

    copy constructor - shallow copy

---
+++ Copy-Constructor/shallowCopy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Pizza {
+public:
+    int* slices;
+
+    Pizza(int s) {
+        slices = new int(s);  
+    }
+
+    
+    Pizza(const Pizza& other) {
+        slices = other.slices;
+    }
+
+    void show(string name) {
+        cout << name << " has " << *slices << " slices at address " << slices << endl;
+    }
+};
+
+int main() {
+    Pizza kid1(8);         
+    Pizza kid2 = kid1;     
+    *kid2.slices = 3;     
+
+    kid1.show("Kid1");    
+    kid2.show("Kid2");     
+}
