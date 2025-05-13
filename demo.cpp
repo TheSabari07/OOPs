5c89501aef6bae7e294cc1f9b58b00706784407a
Sabari Doss R <sabursd18@gmail.com>
2025-05-13 20:47:01 +0530

    basic class and obj

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+    public :
+        string brand;
+        int speed;
+
+        void show(){
+            cout << brand << "is driving at " << speed << "km/h" << endl;
+    }
+};
+
+int main (){
+    Car mycar;
+    mycar.brand = "BMW";
+    mycar.speed = 100;
+    mycar.show();
+    
+}
