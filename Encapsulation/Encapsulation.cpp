b7922636d2a0aa1a149679ba54b072542a43c229
Sabari Doss R <sabursd18@gmail.com>
2025-06-03 22:44:48 +0530

    Encapsulation : Simple encapsulation  code

---
+++ Encapsulation/Encapsulation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+    private:
+        int balance; 
+    
+    public:
+        void deposit(int amount) {
+            if (amount > 0) balance += amount;
+        }
+    
+        int getBalance() {
+            return balance;
+        }
+    };
+    
+    int main() {
+        BankAccount acc;
+        acc.deposit(1000);
+        cout << "Balance: " << acc.getBalance() << endl;
+      
+    }
+    
