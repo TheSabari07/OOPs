06c0dda69c506fd6bcf5d03128e7319d0d9ed379
Sabari Doss R <sabursd18@gmail.com>
2025-06-18 21:56:49 +0530

    Template : Functional Template (base code)

--- Templates/Function_Template.cpp
+++ Templates/Function_Template.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
+int main() {
+    cout << add(2, 3) << endl;        
+    cout << add(2.5, 4.3) << endl;     
+}
